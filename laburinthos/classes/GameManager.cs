241c4f93814a792f3909c08a364e015981427fc0
monoceros123 <Janette.Tetzner@gmx.de>
2024-07-14 12:35:42 +0200

    Ergänzung Übergabe an Lybirinthprinter

--- laburinthos/classes/GameManager.cs
+++ laburinthos/classes/GameManager.cs
@@ -4,6 +4,8 @@ using Avalonia.Input;
 using System.Xml.Serialization;
 using System.Data;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using laburinthos;
+using System.Collections.Generic;
 
 public class GameManager{
     enum Method{
@@ -15,10 +17,18 @@ public class GameManager{
         normal,
         blind
     }
-    public int[] pos;
-    byte size;
+    enum Direction{
+        Up,
+        Down,
+        Right,
+        Left
+    }
+    static Direction way;
+    static int[] pos;
+    static byte size;
 
-    public static void LabyrinthInit(int method, int modus, byte size){
+    public static void LabyrinthInit(int method, int modus, byte Size){
+        size = Size;
         switch (method){
             case 0:
                 BinaryTreeGenerator.GenerateLabyrinth(size);
@@ -37,19 +47,29 @@ public class GameManager{
         switch (direction) {
             case "Up":
                 int[] pos = Spieler.move_up();
+                Direction way = Direction.Up;
                 break;
             case "Left":
                 pos = Spieler.move_left();
+                way = Direction.Left;
                 break;
             case "Down":
                 pos = Spieler.move_down();
+                way = Direction.Down;
                 break;
             case "Right":
                 pos = Spieler.move_right();
+                way = Direction.Right;
                 break;
         }
         //check if player is on end-node:
-        //if (pos[0]==BitConverter.ToInt32(size,0)-1 && pos[1]==BitConverter.ToInt32(size,0)-1){}
+        if (pos[0]==size-1 && pos[1]==size-1){
+            System.Console.WriteLine("Congratulations, you did it. The game is over.");
+        }
+
+        //handover to labyrinthprinter:
+        LabyrinthPrinter.PrintLabyrinthConnection(pos,way);
+
         
     }
 
