df91c954774a76ba66c161cd18c5bb6a50e3494f
Maik <maikmiko77@gmail.com>
2024-07-22 11:13:00 +0200

    Ändere Ordnernamen

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 # Publish for individual runtime
-dotnet publish -c Release -o PublishLinux-x64 --self-contained -r linux-x64
-dotnet publish -c Release -o PublishWindows-x64 --self-contained -r win-x64
+dotnet publish -c Release -o laburinthos-linux-x64 --self-contained -r linux-x64
+dotnet publish -c Release -o laburinthos-windows-x64 --self-contained -r win-x64
 
 # Install zip package if not available
 if [ $(dpkg-query -W -f='${Status}' zip 2>/dev/null | grep -c "ok installed") -eq 0 ];
@@ -9,6 +9,6 @@ then
 fi
 
 # Zip files for download
-zip -r linux-x64.zip ./PublishLinux-x64
-zip -r windows-x64.zip ./PublishWindows-x64
+zip -r laburinthos-linux-x64.zip ./laburinthos-linux-x64
+zip -r laburinthos-windows-x64.zip ./laburinthos-windows-x64
 
